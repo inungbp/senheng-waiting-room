6675f6dc6538e37ad15aba36540a1e9cbf873ec2
inungbp <inung.bagus@sirclo.com>
2023-09-18 15:56:47 +0700

    config

--- app.config.js
+++ app.config.js
@@ -1,5 +1,6 @@
 const storeCode = 'senheng_malay_en_US';
 const intervalCheckStatus = 10000;
+const CMSPageInterest = 'https://senheng-upgrade-pwa.testingnow.me/iphone15-register-of-interest'
 
 module.exports = {
     storeCode,

0e926f47924eff71e644b231f162696dd9e04db3
inungbp <inung.bagus@sirclo.com>
2023-09-19 11:16:00 +0700

    feat([config] waiting - room): config endpoint

--- app.config.js
+++ app.config.js
@@ -1,9 +1,13 @@
 const storeCode = 'senheng_malay_en_US';
 const intervalCheckStatus = 10000;
-const CMSPageInterest = 'https://senheng-upgrade-pwa.testingnow.me/iphone15-register-of-interest'
+const CMSPageInterest = 'https://senheng.testingnow.me/iphone15-register-of-interest';
+const tokenHeader = 'z3rx6lvukw1bds5wsqguw3vs9my5wvdu';
+const endPoint = 'https://mcstaging.senheng.com.my/graphql';
 
 module.exports = {
     storeCode,
     intervalCheckStatus,
-    CMSPageInterest
+    CMSPageInterest,
+    tokenHeader,
+    endPoint,
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,18 @@
 import '@/styles/globals.css'
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { endPoint, tokenHeader } from '@config';
 
 const httpLink = createHttpLink({
-    uri: process.env.NODE_ENV !== 'production'
-        ? 'https://senheng-upgrade-staging.testingnow.me/graphql'
-        : 'https://magento.senheng.com.my/graphql'
+    uri: endPoint,
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = process.env.NODE_ENV !== 'production' 
-    ? 'inwyzt1j98qbg4l130v1iambkhjv1n40'
-    : 'iyubu7xybryry7ky6uqupjr75p2j4z7d';
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ? `Bearer ${tokenHeader}` : "",
     }
   }
 });

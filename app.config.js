1822e5bb47815dd37a73ffcf09357618dcf38f28
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2023-09-21 16:18:21 +0700

    fix preorder cms page interest

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 const storeCode = 'senheng_malay_en_US';
 const intervalCheckStatus = 10000;
-const CMSPageInterest = 'https://www.senheng.com.my//iphone-15-pre-order';
+const CMSPageInterest = 'https://www.senheng.com.my/iphone-15-pre-order';
 const tokenHeader = 'iyubu7xybryry7ky6uqupjr75p2j4z7d';
 const endPoint = 'https://magento.senheng.com.my/graphql';
 

2a60d0d221ef49dd9ed32796bfa66fc6d3effe66
inungbp <inung.bagus@sirclo.com>
2023-09-21 15:36:13 +0700

    feat([config] waiting - room): update config to production

--- app.config.js
+++ app.config.js
@@ -1,8 +1,8 @@
 const storeCode = 'senheng_malay_en_US';
 const intervalCheckStatus = 10000;
-const CMSPageInterest = 'https://senheng.testingnow.me/iphone15-register-of-interest';
-const tokenHeader = 'z3rx6lvukw1bds5wsqguw3vs9my5wvdu';
-const endPoint = 'https://mcstaging.senheng.com.my/graphql';
+const CMSPageInterest = 'https://www.senheng.com.my//iphone-15-pre-order';
+const tokenHeader = 'iyubu7xybryry7ky6uqupjr75p2j4z7d';
+const endPoint = 'https://magento.senheng.com.my/graphql';
 
 module.exports = {
     storeCode,
